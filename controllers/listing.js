8ff92f7f084267920610a565e93292374880a5ac
Ashpak Sayyed <ashpaksayyed1212@gmail.com>
2024-03-19 19:57:39 +0530

    first

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,4 +1,7 @@
 const Listing = require("../models/listing.js");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
     const allListings = await Listing.find({});
@@ -22,12 +25,22 @@ module.exports.showListing = async (req, res) => {
 
 
 module.exports.createListing = async (req, res, next) => {
+  let response =  await geocodingClient.forwardGeocode({
+        query: req.body.listings.location,
+        limit: 1,
+      })
+        .send();
+
     let url = req.file.path;
     let filename = req.file.filename;
     const newListing = new Listing(req.body.listings);
     newListing.owner = req.user._id;
     newListing.image = { url, filename };
-    await newListing.save();
+
+    newListing.geometry = response.body.features[0].geometry;
+
+   let savedListing = await newListing.save();
+   console.log(savedListing);
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 };
